10fbf151241e2fadd421df03a43dd17f0b88fb68
alain-ateh <mrmeloateh@yahoo.com>
2025-07-28 06:35:07 -0400

    backend.tf commit

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "ateh-terraform-state-bucket"
     key            = "dev/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locks"
