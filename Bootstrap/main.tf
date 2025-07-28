0a13a59ae5ef657e9f5cbb11cd298176640cd4e1
alain-ateh <mrmeloateh@yahoo.com>
2025-07-28 06:36:37 -0400

    ../../Bootstrap/main.tf commit

--- Bootstrap/main.tf
+++ Bootstrap/main.tf
@@ -26,19 +26,19 @@ resource "aws_s3_bucket" "terraform_state" {
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_encryption" "terraform_state" {
+# Enable encryption on the bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
@@ -54,9 +54,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name           = var.dynamodb_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
@@ -73,12 +73,13 @@ resource "aws_dynamodb_table" "terraform_locks" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "state_bucket_name" {
-  description = "Name of the S3 bucket for Terraform state"
+  description = "Globally unique name of the S3 bucket for Terraform state"
   type        = string
+  default     = "ateh-terraform-state-bucket"  # Static unique name
 }
 
 variable "dynamodb_table_name" {
